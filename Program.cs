bc2713eacec32e1ebfca3fec4fcafed347f93482
Joe Sun <dps_swanuk@hotmail.com>
2023-07-07 00:25:02 +0200

    create dotnet console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoundexMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
